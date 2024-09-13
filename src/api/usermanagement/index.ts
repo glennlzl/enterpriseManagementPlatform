43c2424bd7f6abd91f0f08191324cca0f054f030
glennlzl <glennli616@gmail.com>
2024-09-12 23:27:24 -0700

    fix more bugs

--- src/api/usermanagement/index.ts
+++ src/api/usermanagement/index.ts
@@ -67,7 +67,7 @@ export interface EmployeeInfoAddOrUpdateRequest {
 }
 // http://47.93.51.8/user/login
 export const GENERAL_API_BASE_URL = 'http://47.93.51.8:8081/api';
-export const GENERAL_CLIENT_API_BASE_URL = 'http://47.93.51.8//';
+export const GENERAL_CLIENT_API_BASE_URL = 'http://47.93.51.8';
 
 export const API_BASE_URL = `${GENERAL_API_BASE_URL}`;
 
--- src/hooks/login/Hook.useDingTalkLogin.tsx
+++ src/hooks/login/Hook.useDingTalkLogin.tsx
@@ -12,7 +12,7 @@ export const hookUseDingTalkLogin = () => {
   const navigate = useNavigate();
 
   const handleDingTalkLogin = () => {
-    const redirectUri = encodeURIComponent('http://47.93.51.8//user/login');
+    const redirectUri = encodeURIComponent('http://47.93.51.8/user/login');
     const client_id = 'dingwjfj1sx02steekvm';
     window.location.href = `https://login.dingtalk.com/oauth2/auth?redirect_uri=${redirectUri}&response_type=code&client_id=${client_id}&scope=openid&state=dddd&prompt=consent`;
   };
