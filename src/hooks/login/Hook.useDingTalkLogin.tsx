2fb64b4e1d848ebbfa6a821b5a33cb0d07a2e638
glennlzl <glennli616@gmail.com>
2024-10-20 20:28:00 -0700

    fixed more bugs

--- src/hooks/login/Hook.useDingTalkLogin.tsx
+++ src/hooks/login/Hook.useDingTalkLogin.tsx
@@ -13,10 +13,9 @@ export const hookUseDingTalkLogin = () => {
 
   const handleDingTalkLogin = () => {
     const redirectUri = encodeURIComponent('http://47.93.51.8/user/login');
-    const client_id = 'dingwjfj1sx02steekvm';
+    const client_id = 'dingwy6hwk0dkskuft0m';
     window.location.href = `https://login.dingtalk.com/oauth2/auth?redirect_uri=${redirectUri}&response_type=code&client_id=${client_id}&scope=openid&state=dddd&prompt=consent`;
   };
-
 
   useEffect(() => {
     const urlParams = new URL(window.location.href);
